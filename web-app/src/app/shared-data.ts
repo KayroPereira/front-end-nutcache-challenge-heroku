d7ec2cb23eb96161ec9cbe5b66dd2575107bf5e4
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-08-30 13:05:02 -0300

    Heroku configuration

--- web-app/src/app/shared-data.ts
+++ web-app/src/app/shared-data.ts
@@ -1,3 +1,4 @@
 export class SharedData{
-  public static API_ENDPOINT = "http://localhost:8080/api/v1/";
+  // public static API_ENDPOINT = "http://localhost:8080/api/v1/";
+  public static API_ENDPOINT = "https://nutcachechallenge-kayropereira.herokuapp.com/api/v1/";
 }
